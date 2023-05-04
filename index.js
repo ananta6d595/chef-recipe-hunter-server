b406a3ab544e4e704a9d286c89907c716cb6bc62
Ananta Roy <ananta6d595@gmail.com>
2023-05-04 18:29:24 +0600

    added individual recipe fetch functionality

--- index.js
+++ index.js
@@ -17,9 +17,27 @@ app.get('/chefs', (req, res) => {
     res.send(chef)
 })
 
+app.get('/chefs/:id', (req, res) => {
+
+    const reqId = parseInt(req.params.id);
+    const expectedChef = chef.find(cf => cf.id === reqId)
+    // console.log(expectedChef );
+    res.send(expectedChef)
+})
+
 app.get('/recipes', (req, res) => {
     res.send(recipes)
 })
+
+app.get('/recipe/:id', (req, res) => {
+
+    const reqId = parseInt(req.params.id);
+    const expectedRecipe = recipes.recipes.find(cf => cf.id === reqId)
+    console.log(expectedRecipe );
+    res.send(expectedRecipe)
+
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
